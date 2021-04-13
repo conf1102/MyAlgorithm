3bb1ba0d990f694a68736952c2c8c0bdfdba0ba2
Alvin <conf1102@163.com>
2021-04-13 16:02:47 +0800

    位运算基础

---
+++ NewlyClass/src/main/java/com/alvin/Code01_PrintB.java
@@ -0,0 +1,26 @@
+package com.alvin;
+
+public class Code01_PrintB {
+    public static void main(String[] args) {
+//        int num = 4;
+//        //整形32位，long为64位
+//        print(num);
+//        int test = 4;
+//        print(test);
+//        print(test<<1);
+//        print(test<<2);
+//        print(test<<8);
+        int a = 4;
+        //取反
+        print(~a);
+
+
+    }
+
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+}
