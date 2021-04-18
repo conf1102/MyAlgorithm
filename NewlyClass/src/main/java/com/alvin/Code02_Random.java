eb43b632842de9e7b3589de2209b5f85f0b40f84
Alvin <conf1102@163.com>
2021-04-18 14:02:58 +0800

    class05 提交

---
+++ NewlyClass/src/main/java/com/alvin/Code02_Random.java
@@ -0,0 +1,11 @@
+package com.alvin;
+
+public class Code02_Random {
+    public static void main(String[] args) {
+        System.out.println(f());
+    }
+
+    public static double f(){
+        return Math.random() * 5;
+    }
+}
---
+++ NewlyClass/src/main/java/com/alvin/class05/Code_BitMap.java
@@ -0,0 +1,8 @@
+package com.alvin.class05;
+
+public class Code_BitMap {
+    public static void main(String[] args) {
+        //1.右移6位就是除64
+
+    }
+}
