222e4268136640dfc7385b9dcd97613c103e869b
Alvin <conf1102@163.com>
2021-04-29 00:56:10 +0800

    选择排序

---
+++ SystemClass/src/main/java/class01/SelectSort.java
@@ -0,0 +1,42 @@
+package class01;
+
+public class SelectSort {
+
+    //将一个无序数组排序，算法如下
+    //在 0 ... N-1 遍历寻找最小值，找到并且和 0 位置的数互换位置
+    //在 1 ... N-1 遍历寻找最小值，找到并且和 1 位置的数互换位置
+    //在 2 ... N-1 遍历寻找最小值，找到并且和 2 位置的数互换位置
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            switchPosition(arr, i, minIndex);
+        }
+
+
+    }
+
+    public static void switchPosition(int[] arr, int pos1, int pos2) {
+        int pos1Value = arr[pos1];
+        int pos2Value = arr[pos2];
+        arr[pos1] = pos2Value;
+        arr[pos2] = pos1Value;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 5, 2, 8, 9};
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
